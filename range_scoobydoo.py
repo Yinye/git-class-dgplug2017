53a7ef0eb1cb3192b43ab8b309d04ac71bcf0d6c
yinye <onyinyemadubuko@gmail.com>
2017-09-13 16:48:24 +0100

    scobby doo

---
+++ range_scoobydoo.py
@@ -0,0 +1,10 @@
+##### Scooby Doo ####
+
+### multiples of 3 return Scooby while multiples of 7 return Doo
+for n in range (1, 100, 1):
+  if n % 3 == 0:            
+      print("Scooby")
+  elif n % 7 == 0:
+      print("Doo")
+  else :
+      print(n)
